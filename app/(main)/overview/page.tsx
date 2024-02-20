4345da1e42d011ea4994d509c4d08a05be88c86e
sjia0047 <shen.jng1@gmail.com>
2024-02-20 18:09:12 +1100

    add description to tasks page

--- app/(main)/overview/page.tsx
+++ app/(main)/overview/page.tsx
@@ -3,6 +3,7 @@ export default function Overview() {
   return (
     <div className="p-6 ml-6">
       <h1 className="text-white text-4xl font-semibold">Tasks</h1>
+      <p className="text-[#D6C5E8] text-sm">Add and see remaining tasks</p>
     </div>
   );
 }
