c704738c4f0a3d1d9892140a18a98e2bf4e04a65
sjia0047 <shen.jng1@gmail.com>
2024-02-21 00:25:58 +1100

    padding changes to main page (overview, shop)

--- app/(main)/overview/page.tsx
+++ app/(main)/overview/page.tsx
@@ -2,9 +2,9 @@ import TaskTable from "@/app/ui/main/overview/task-table";
 
 export default function Overview() {
   return (
-    <>
+    <div className="py-4 pr-3">
       <h1 className="text-white text-4xl font-semibold mb-1">Overview</h1>
       <p className="text-[#D6C5E8] text-sm mb-8">See earnings history</p>
-    </>
+    </div>
   );
 }
--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Shop() {
   return (
-    <>
+    <div className="py-4 pr-3">
       <h1 className="text-white text-4xl font-semibold mb-1">Shop</h1>
       <p className="text-[#D6C5E8] text-sm mb-8">Spend coins for guilt-free free time</p>
-    </>
+    </div>
   );
 }
