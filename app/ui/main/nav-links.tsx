9288d967e81d88cba2d5431029a133de1a28bc33
sjia0047 <shen.jng1@gmail.com>
2024-02-20 15:48:44 +1100

    refactor: extract nav links into component

--- app/ui/main/nav-links.tsx
+++ app/ui/main/nav-links.tsx
@@ -1,4 +1,34 @@
+import Link from "next/link";
+import { GrOverview } from "react-icons/gr";
+import { TbMoneybag } from "react-icons/tb";
+
+interface NavLink {
+  name: string;
+  href: string;
+  icon: React.ReactElement;
+}
+
+const links: Array<NavLink> = [
+  { name: "Overview", href: "/overview", icon: <GrOverview /> },
+  { name: "Shop", href: "/shop", icon: <TbMoneybag /> },
+];
 
 export default function NavLinks() {
-  
+  return (
+    <>
+      {/* links */}
+      {links.map((link) => {
+        return (
+          <div key={link.name} className="flex mb-3 pl-4 py-1 text-sm text-white bg-[#474F7A] rounded-lg items-center">
+            <div className="mr-2">{link.icon}</div>
+            <Link href={link.href}>{link.name}</Link>
+          </div>
+        );
+      })}
+    </>
+  );
 }
+
+// Self Notes
+// type vs interface https://stackoverflow.com/questions/36782896/in-typescript-what-is-the-difference-between-type-and-interface
+// https://www.totaltypescript.com/where-to-put-your-types-in-application-code
--- app/ui/main/side-nav.tsx
+++ app/ui/main/side-nav.tsx
@@ -1,34 +1,13 @@
-import { GrOverview } from "react-icons/gr";
-import { TbMoneybag } from "react-icons/tb";
 import Image from "next/image";
-import Link from "next/link";
 import { FaRegUserCircle } from "react-icons/fa";
-
-interface NavLink {
-  name: string;
-  href: string;
-  icon: React.ReactElement;
-}
-
-const links: Array<NavLink> = [
-  { name: "Overview", href: "/main/overview", icon: <GrOverview /> },
-  { name: "Shop", href: "/main/shop", icon: <TbMoneybag /> },
-];
+import NavLinks from "./nav-links";
 
 export default function SideNav() {
   return (
     <div className="flex flex-col px-4 pt-4 pb-7 min-h-screen">
       <Image src="/motivate-logo.png" className="w-40" width={801} height={806} alt="Motivate logo" />
       <div className="grow">
-        {/* links */}
-        {links.map((link) => {
-          return (
-            <div key={link.name} className="flex mb-3 pl-4 py-1 text-sm text-white bg-[#474F7A] rounded-lg items-center">
-              <div className="mr-2">{link.icon}</div>
-              <Link href={link.href}>{link.name}</Link>
-            </div>
-          );
-        })}
+        <NavLinks />
       </div>
       <div>
         <p className="text-[13px] text-[#FFD0EC] mb-2">Sign Out</p>
@@ -46,5 +25,4 @@ export default function SideNav() {
 
 // Self Notes
 // https://www.w3.org/TR/css-flexbox-1/#auto-margins. remember you need a height for mb-auto/grow to work
-// type vs interface https://stackoverflow.com/questions/36782896/in-typescript-what-is-the-difference-between-type-and-interface
-// https://www.totaltypescript.com/where-to-put-your-types-in-application-code
+
