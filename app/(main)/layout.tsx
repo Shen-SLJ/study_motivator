c4e5d7cfeb6169f78171529cf47a9f24bb977cf8
sjia0047 <shen.jng1@gmail.com>
2024-02-21 00:22:26 +1100

    refactor: padding changes to all pages

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,7 @@ import SideNav from "@/app/ui/main/side-nav";
  */
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex bg-[#1F2544] px-5 pt-6 pb-7 min-h-screen">
+    <div className="flex bg-[#1F2544] pl-5 pr-8 pt-6 pb-7 min-h-screen">
       <div className="mr-16">
         <SideNav />
       </div>
--- app/(main)/tasks/page.tsx
+++ app/(main)/tasks/page.tsx
@@ -2,7 +2,7 @@ import TaskTable from "@/app/ui/main/overview/task-table";
 
 export default function Tasks() {
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-full py-4 pr-3">
       <h1 className="text-white text-4xl font-semibold mb-1">Tasks</h1>
       <p className="text-[#D6C5E8] text-sm mb-8">Add and see remaining tasks</p>
       <div className="grow"><TaskTable /></div>
