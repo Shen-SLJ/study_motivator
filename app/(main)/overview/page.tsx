c43c74e69ad90cf9dcf44d9f55bc8aad2b33ba49
sjia0047 <shen.jng1@gmail.com>
2024-02-20 16:02:24 +1100

    refactor: padding changes

--- app/(main)/overview/page.tsx
+++ app/(main)/overview/page.tsx
@@ -2,6 +2,8 @@ import SideNav from "@/app/ui/main/side-nav";
 
 export default function Main() {
   return (
-    <p>Hello World</p>
-  )
+    <div className="p-6">
+      <h1 className="text-white text-4xl font-semibold">Tasks</h1>
+    </div>
+  );
 }
