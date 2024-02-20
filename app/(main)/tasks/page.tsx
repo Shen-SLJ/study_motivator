9a5b879b032a915193e40423d4988b6e7d2877a9
sjia0047 <shen.jng1@gmail.com>
2024-02-21 00:30:10 +1100

    padding for tasks page

--- app/(main)/tasks/page.tsx
+++ app/(main)/tasks/page.tsx
@@ -2,7 +2,7 @@ import TaskTable from "@/app/ui/main/overview/task-table";
 
 export default function Tasks() {
   return (
-    <div className="flex flex-col h-full py-4 pr-3">
+    <div className="flex flex-col h-full pt-4 pb-6 pr-6">
       <h1 className="text-white text-4xl font-semibold mb-1">Tasks</h1>
       <p className="text-[#D6C5E8] text-sm mb-8">Add and see remaining tasks</p>
       <div className="grow"><TaskTable /></div>
