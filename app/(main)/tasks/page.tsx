667879e15b55bb2f09ae54e0a1600743ca74df06
sjia0047 <shen.jng1@gmail.com>
2024-02-20 23:37:11 +1100

    table scaffold

--- app/(main)/tasks/page.tsx
+++ app/(main)/tasks/page.tsx
@@ -2,10 +2,10 @@ import TaskTable from "@/app/ui/main/overview/task-table";
 
 export default function Tasks() {
   return (
-    <div className="p-6 ml-6">
+    <div className="flex flex-col h-full p-6 ml-6">
       <h1 className="text-white text-4xl font-semibold mb-1">Tasks</h1>
       <p className="text-[#D6C5E8] text-sm mb-8">Add and see remaining tasks</p>
-      <TaskTable />
+      <div className="grow"><TaskTable /></div>
     </div>
   );
 }
--- app/ui/main/overview/task-table.tsx
+++ app/ui/main/overview/task-table.tsx
@@ -1,6 +1,16 @@
 
+/**
+ * Task table. Table frows to fill remaining available space of container.
+ */
 export default function TaskTable() {
   return (
-    <p>Hi</p>
+    <div className="flex flex-col h-full">
+      <div className="px-5 py-2 mb-5 rounded-lg text-[#FFD0EC] bg-[#2D3662] w-4/12 min-w-52">Search</div>
+      <div className="h-full bg-[#2D3662] rounded-lg"></div>
+    </div>
   )
 }
+
+// Self Notes
+// We need flex flex-col for top div, otherwise h-full will cause table overshoot at bottom of page due to 
+// not accounting for search bar height (h-full uses 100% of parent div's height value as px)
