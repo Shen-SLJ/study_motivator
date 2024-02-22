000d50927d08047e023e27ca723038c9c7f1f2e0
sjia0047 <shen.jng1@gmail.com>
2024-02-22 17:48:01 +1100

    force dynamic tasks fetching

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,5 +1,7 @@
 import { sql } from '@vercel/postgres'
 
+export const dynamic = 'force-dynamic'
+
 export type Task = {
   id: string;
   description: string;
@@ -16,3 +18,7 @@ export async function GET() {
 
   return Response.json(tasks.rows)
 }
+
+// Self Notes
+// Why stale data? https://github.com/orgs/vercel/discussions/4696. Reason for export const dynamic here
+// -Check here https://nextjs.org/learn/dashboard-app/static-and-dynamic-rendering making the dashboard dynamic
